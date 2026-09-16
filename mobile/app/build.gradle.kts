29d1413b6164c7879c0988304f8cd99633a21429
zikoallord1 <zikoallord@gmail.com>
2026-09-15 17:22:36 -0700

    mobile: fix APK compile SDK for Media3 RTSP

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.zikoallord.grocerymobile"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.zikoallord.grocerymobile"
