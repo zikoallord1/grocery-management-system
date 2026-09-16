e7c743ab81bd1ef4946e4c2a9e7c9d8a4c96fdf9
zikoallord1 <zikoallord@gmail.com>
2026-09-15 17:53:14 -0700

    feat: add Android app build configuration

---
+++ mobile/app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.zikoallord.grocerymobile"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.zikoallord.grocerymobile"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.media3:media3-exoplayer:1.11.0")
+    implementation("androidx.media3:media3-exoplayer-rtsp:1.11.0")
+    implementation("androidx.media3:media3-ui:1.11.0")
+}
